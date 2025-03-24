7eb5f92402a948ee6bcf9397f60ef11daaeb1ae7
Taylor Masiello <taylor.masiello@outlook.com>
2025-03-24 14:25:28 -0400

    styling

--- src/App.js
+++ src/App.js
@@ -4,16 +4,31 @@ import React, { useState } from "react";
 import './App.css';
 
 function App() {
+  const [pokemonList, setPokemonList] = useState(pokemonData.results);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [selectedPokemon, setSelectedPokemon] = useState(null);
+
+  //returns list of pokemon from pokeapi.json, filtered by search box input
+  const filteredPokemonList = pokemonList.filter((pokemon) => pokemon.name.includes(searchTerm));
+  
   return (
     <div className="App">
       <header>
         <img alt="pokeLogo" className="logo" src={logo} />
       </header>
-
       <main>
+
         <div className="search-container">
           <input className="search-box" type="text" placeholder="Search..." />
+          <ul>
+            {filteredPokemonList.map((pokemon => (
+              <li key={pokemon.id} className="pokemon-item">
+                <a href="#">{pokemon.name}</a>
+              </li>
+            )))}
+          </ul>
         </div>
+        
       </main>
     </div>
   );

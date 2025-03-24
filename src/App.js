9f970f6d2b201cfb8501734ef57e8ec807875c8f
Taylor Masiello <taylor.masiello@outlook.com>
2025-03-24 14:40:44 -0400

    logic to display additional poke info + more styling

--- src/App.js
+++ src/App.js
@@ -4,30 +4,66 @@ import React, { useState } from "react";
 import './App.css';
 
 function App() {
+  {/* useState: React Hook that enables update to a certain variable */}
   const [pokemonList, setPokemonList] = useState(pokemonData.results);
   const [searchTerm, setSearchTerm] = useState("");
   const [selectedPokemon, setSelectedPokemon] = useState(null);
 
   //returns list of pokemon from pokeapi.json, filtered by search box input
   const filteredPokemonList = pokemonList.filter((pokemon) => pokemon.name.includes(searchTerm));
+
+  const showPokemon = async (url) => {
+    const response = await fetch(url);
+    if(!response.ok){
+      console.error('Error fetching Pokemon: ${response.statusText}');
+      return;
+    }
+
+    const data = await response.json();
+    setSelectedPokemon(data);
+  }
   
   return (
     <div className="App">
       <header>
         <img alt="pokeLogo" className="logo" src={logo} />
       </header>
-      <main>
 
+      <main>
         <div className="search-container">
           <input className="search-box" type="text" placeholder="Search..." />
+          </div>
+
+          {/* selectedPokemon && so that the line {{ selectedPokemon.name }} doesn't throw an error for trying to access a property that doesn't exist on the object selectedPokemon */
+          /* selectedPokemon is initialized as null in the beginning: const [selectedPokemon, setSelectedPokemon] = useState(null);)*/}
+          {selectedPokemon && (
+            <div className="pokemon-details">
+              <h2>{selectedPokemon.name}</h2>
+              <img
+                src={selectedPokemon.sprites.font_default}
+                alt={selectedPokemon.name}
+              />
+              <p>Height: {selectedPokemon.height}</p>
+              <p>Weight: {selectedPokemon.weight}</p>
+
+              {/* using map to loop through elements of stats array and output name + base_state props */}
+              {selectedPokemon.stats.map((stat, index) => (
+                <div key={index}>
+                  <p>
+                    {stat.stat.name}: {stat.base_stat}
+                  </p>
+                </div>
+              ))}
+            </div>
+          )}
+
           <ul>
             {filteredPokemonList.map((pokemon => (
               <li key={pokemon.id} className="pokemon-item">
-                <a href="#">{pokemon.name}</a>
+                <a href="#" onClick={() => showPokemon(pokemon.url)}>{pokemon.name}</a>
               </li>
             )))}
           </ul>
-        </div>
         
       </main>
     </div>
